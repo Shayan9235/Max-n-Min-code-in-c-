ad5434023d19559bb366f05efbde6b25ce8b0d65
shayan <shayan@UNKNOWN>
2023-05-29 12:31:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Transactions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("enter a number: ");
+        int number1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter another number: ");
+        int number2 = int.Parse(Console.ReadLine());
+        int max = Math.Max(number1, number2);
+        Console.WriteLine(max);
+
+
+        }
+        
+}
